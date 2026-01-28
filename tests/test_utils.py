37d41368572ee52bc842b92863190a8d86761ef1
agitfirat <157006017+agitfirat@users.noreply.github.com>
2026-01-28 16:44:42 +0100

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -21,20 +21,20 @@ def test_count_lines_from_string(text: str, expected: int) -> None:
     assert count_lines_from_string(text) == expected
 
 
-@given(
-    lines=st.lists(st.text(min_size=0, max_size=10), max_size=10),
-    add_trailing_newline=st.booleans(),
-)
-def test_count_lines_from_file_hypothesis(lines: list[str], add_trailing_newline: bool) -> None:
-    text = "\n".join(lines)
-    if add_trailing_newline and lines:
-        text = f"{text}\n"
-    elif add_trailing_newline and not lines:
-        text = "\n"
+#@given(
+    #lines=st.lists(st.text(min_size=0, max_size=10), max_size=10),
+    #add_trailing_newline=st.booleans(),
+#)
+#def test_count_lines_from_file_hypothesis(lines: list[str], add_trailing_newline: bool) -> None:
+#    text = "\n".join(lines)
+#   if add_trailing_newline and lines:
+#        text = f"{text}\n"
+#    elif add_trailing_newline and not lines:
+#        text = "\n"
 
-    expected = len(text.splitlines())
+#    expected = len(text.splitlines())
 
-    with tempfile.NamedTemporaryFile(mode="w", delete=True, suffix=".txt", encoding = "utf-8") as tmp:
-        tmp.write(text)
-        tmp.flush()
-        assert count_lines_from_file(tmp.name) == expected
+#    with tempfile.NamedTemporaryFile(mode="w", delete=True, suffix=".txt", encoding = "utf-8") as tmp:
+#        tmp.write(text)
+#        tmp.flush()
+#        assert count_lines_from_file(tmp.name) == expected
